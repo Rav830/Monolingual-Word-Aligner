edff9f2c9d4996221423a3b6a355bc843c39d0c8
Rav830 <ravneetsingh830@gmail.com>
2020-01-13 13:50:13 -0500

    added a server start script

---
+++ startServer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -mx4g -cp "./CoreNLP_Setup/stanford-corenlp-full-2017-06-09/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -preload tokenize,ssplit,pos,lemma,ner,depparse -port 9000 -timeout 15000
